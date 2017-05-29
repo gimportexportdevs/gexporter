9e73b837de05b249826350cb290e7dc70bcbd312
Harald Hoyer <harald@redhat.com>
2017-05-29 13:15:00 +0200

    add walking pace grade factor function

--- app/src/main/java/org/surfsite/gexporter/Gpx2Fit.java
+++ app/src/main/java/org/surfsite/gexporter/Gpx2Fit.java
@@ -281,13 +281,24 @@ public class Gpx2Fit {
         return courseName;
     }
 
+    /**
+     * Grade adjusted pace based on a study by Alberto E. Minetti on the energy cost of
+     * walking and running at extreme slopes.
+     *
+     * see Minetti, A. E. et al. (2002). Energy cost of walking and running at extreme uphill and downhill slopes.
+     *      Journal of Applied Physiology 93, 1039-1046, http://jap.physiology.org/content/93/3/1039.full
+     */
+    public double getWalkingGradeFactor(double g)
+    {
+        return 1.0 + (g * (19.5 + g * (46.3 + g * (-43.3 + g * (-30.4 + g * 155.4))))) / 3.6;
+    }
+
     public void writeFit(File outfile) {
         WayPoint last = null;
         double minEle = Double.NaN;
         double maxEle = Double.NaN;
         double totalAsc = Double.NaN;
         double totalDesc = Double.NaN;
-        long i = 0;
         double dist = .0;
         double sdist = .0;
         double lcdist = .0;
